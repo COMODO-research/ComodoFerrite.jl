8ef43c69210c4366a99289b72fc66a4a56a7c85c
Documenter.jl <documenter@juliadocs.github.io>
2025-09-05 19:54:23 +0000

    build based on b0ae5f8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ComodoFerrite","page":"Home","title":"ComodoFerrite","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ComodoFerrite.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
