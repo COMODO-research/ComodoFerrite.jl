5c299aad31b057d5f16a14dab14144ee875a5871
Aminofa70 <amin.alibakhshi@upm.es>
2025-09-07 15:35:59 +0200

    add nodes & cells for quad4, tri3, tet4, hex8

--- src/functions.jl
+++ src/functions.jl
@@ -24,4 +24,60 @@ Name of fucntion:
 CTF:  ComodoToFerrite (can be changed)
 """
 ###########################################################
-###### Convert Quadrilateral ##############################
+###########################################################
+"""
+      ComodoToFerrite(F1, V1, Ferrite.Quadrilateral)
+
+Convert Quad4 of Comodo.jl to Ferrite.Quadrilateral mesh in Ferrite.jl
+
+"""
+function ComodoToFerrite(F1, V1, ::Type{Ferrite.Quadrilateral})
+
+    cells = [Ferrite.Quadrilateral((e[1], e[2], e[3], e[4])) for e in F1]
+    nodes = [Ferrite.Node((e[1], e[2])) for e in V1]
+
+    return Grid(cells, nodes)
+end
+
+###########################################################
+###########################################################
+"""
+      ComodoToFerrite(F1, V1,Ferrite.Triangle)
+
+Convert Tri3 of Comodo.jl to Ferrite.Triangle mesh in Ferrite.jl
+"""
+function ComodoToFerrite(F1, V1, ::Type{Ferrite.Triangle})
+
+    cells = [Ferrite.Triangle((e[1], e[2], e[3])) for e in F1]
+    nodes = [Ferrite.Node((e[1], e[2])) for e in V1]
+
+    return Grid(cells, nodes)
+end
+###########################################################
+###########################################################
+"""
+     ComodoToFerrite(E, V, Ferrite.Hexahedron )
+
+Convert Hex8 of Comodo.jl to Ferrite.Hexahedron mesh in Ferrite.jl
+"""
+function ComodoToFerrite(E, V, ::Type{Ferrite.Hexahedron})
+
+    cells = [Ferrite.Hexahedron((e[1], e[2], e[3], e[4], e[5], e[6], e[7], e[8])) for e in E]
+    nodes = [Ferrite.Node((e[1], e[2], e[3])) for e in V]
+
+    return Grid(cells, nodes)
+end
+###########################################################
+###########################################################
+"""
+      ComodoToFerrite(E, V, Ferrite.Tetrahedron)
+
+Convert Tet4 of Comodo.jl to Ferrite.Tetrahedron mesh in Ferrite.jl
+"""
+function ComodoToFerrite(E, V, ::Type{Ferrite.Tetrahedron})
+
+    cells = [Ferrite.Tetrahedron((e[1], e[2], e[3], e[4])) for e in E]
+    nodes = [Ferrite.Node((e[1], e[2], e[3])) for e in V]
+
+    return Grid(cells, nodes)
+end
